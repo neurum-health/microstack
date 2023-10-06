eff2151a0867a4c55587d361336c61769a65d747
Zhang <40197685+ZhangMing-K@users.noreply.github.com>
2023-10-06 01:57:12 +0000

    Add support for boolean in query params

--- src/helpers.js
+++ src/helpers.js
@@ -1,21 +1,29 @@
-const qs = require("qs")
+const qs = require("qs");
 
+function isBoolean(val) {
+  return val === "false" || val === "true";
+}
+
+function parseBoolean(val) {
+  return val === "true";
+}
 
 module.exports.parseQueryString = (str) => {
-    let params = qs.parse(str, {
-        allowPrototypes: true,
-        parseArrays: true
-    })
+  let params = qs.parse(str, {
+    allowPrototypes: true,
+    parseArrays: true,
+  });
 
-    for(let key in params)
-    {
-        const value = params[key]
-        
-        if(typeof value === "object")
-        {
-            params[key] = Object.values(value)
-        }
+  for (let key in params) {
+    const value = params[key];
+
+    if (isBoolean(value)) {
+      params[key] = parseBoolean(value);
     }
 
-    return params
-}
+    if (typeof value === "object") {
+      params[key] = Object.values(value);
+    }
+  }
+  return params;
+};
